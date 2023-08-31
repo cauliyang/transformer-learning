89a04e5a3410ea938cf905461c2f34c66fef3fe2
Yangyang Li <yangyang.li@northwestern.edu>
2023-08-31 16:48:12 -0500

    use precommit

--- src/transformer/example.py
+++ src/transformer/example.py
@@ -1,26 +1,10 @@
-from os.path import exists
+import functools
 import torch
-import torch.nn as nn
-from torch.nn.functional import log_softmax, pad
-import math
-import copy
-import time
-from torch.optim.lr_scheduler import LambdaLR
 import pandas as pd
 import altair as alt
 
-from torchtext.data.functional import to_map_style_dataset
-from torchtext.vocab import build_vocab_from_iterator
-import torchtext.datasets as datasets
 
-from torch.utils.data import DataLoader
-import spacy
-import GPUtil
 import warnings
-from torch.utils.data.distributed import DistributedSampler
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 
 from pathlib import Path
 
@@ -49,29 +33,26 @@ def execute_example(fn, args=[]):
 class DummyOptimizer(torch.optim.Optimizer):
     def __init__(self):
         self.param_groups = [{"lr": 0}]
-        None
 
     def step(self):
-        None
+        pass
 
     def zero_grad(self, set_to_none=False):
-        None
+        pass
 
 
 class DummyScheduler:
     def step(self):
-        None
-
-
-import functools
+        pass
 
 
 def save_char(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        char = func(*args, **kwargs)
-        char.save(FIGURE_PATH / f"{func.__name__}.html")
-        return char
+        if RUN_EXAMPLES:
+            char = func(*args, **kwargs)
+            char.save(FIGURE_PATH / f"{func.__name__}.html")
+            return char
 
     return wrapper
 
--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -1,26 +1,8 @@
-from os.path import exists
 import torch
 import torch.nn as nn
-from torch.nn.functional import log_softmax, pad
+from torch.nn.functional import log_softmax
 import math
 import copy
-import time
-from torch.optim.lr_scheduler import LambdaLR
-import pandas as pd
-import altair as alt
-
-from torchtext.data.functional import to_map_style_dataset
-from torchtext.vocab import build_vocab_from_iterator
-import torchtext.datasets as datasets
-
-from torch.utils.data import DataLoader
-import spacy
-import GPUtil
-import warnings
-from torch.utils.data.distributed import DistributedSampler
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 
 
 class EncoderDecoder(nn.Module):
